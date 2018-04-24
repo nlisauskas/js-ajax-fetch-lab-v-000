d49ef62d3abb24d8fe0ce7ad58c0c8ac916ed2b0
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-24 01:40:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
+const token = '62ca5ef9c2883fbedb54924265b50e8d77023cc3';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
